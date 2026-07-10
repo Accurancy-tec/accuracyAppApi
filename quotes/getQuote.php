1d37c0ecf985cc7d234770a3d81d0ea0fa9bf6bf
MuriloGoms <murilogcordeiro08@gmail.com>
2026-07-10 19:04:55 -0300

    fix: Corrigi o fluxo da api

--- quotes/getQuote.php
+++ quotes/getQuote.php
@@ -2,7 +2,16 @@
 
 require_once '../service/BrapiService.php';
 
-$ticker = $_GET['ticker'];
+$ticker = $_GET['symbol'];
+
+
+if(empty($ticker)){
+    echo json_encode([
+        "success" => false,
+        "message" => "Ticker não informado"
+    ]);
+    exit();
+}
 
 $reponse = buscarAcao($ticker);
 
